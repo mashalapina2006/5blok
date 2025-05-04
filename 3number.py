09b85fdd15d8c329d501443d5648e8b3133dc26d
mashalapina2006 <marylapina01@yandex.ru>
2025-05-05 00:24:30 +0300

    done

---
+++ 3number.py
@@ -0,0 +1,28 @@
+def countLines(file_obj):
+    lines = file_obj.readlines()
+    return len(lines)
+
+def countChars(file_obj):
+    text = file_obj.read()
+    return len(text)
+
+def test(module_obj):
+    try:
+        file_name = module_obj.__file__
+        with open(file_name, 'r', encoding='utf-8') as file:
+            num_lines = countLines(file)
+            file.seek(0)
+            num_chars = countChars(file)
+
+        if num_lines is not None and num_chars is not None:
+            print(f"Файл: {file_name}")
+            print(f"Количество строк: {num_lines}")
+            print(f"Количество символов: {num_chars}")
+    except FileNotFoundError:
+        print(f"Файл '{file_name}' не найден.")
+    except Exception as e:
+        print(f"Общая ошибка: {e}")
+
+if __name__ == '__main__':
+    import mymod
+    test(mymod)
