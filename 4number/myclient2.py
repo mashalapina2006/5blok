2dc4154833ab1f00bf5b7ce7e44dc7deb2b11e7f
mashalapina2006 <marylapina01@yandex.ru>
2025-05-05 00:34:51 +0300

    done

---
+++ 4number/myclient2.py
@@ -0,0 +1,7 @@
+import mymod
+
+def run_test():
+    mymod.test(mymod)
+    
+if __name__ == '__main__':
+    run_test()
---
+++ 4number/pppppp.py
@@ -0,0 +1,8 @@
+from mymod import countLines, countChars, test
+import mymod  
+
+def run_test():
+    test(mymod)  
+
+if __name__ == '__main__':
+    run_test()
